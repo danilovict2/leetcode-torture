3f47a54acdef7e560012cb6dbfeb01cd7e6b4759
Danilo Sindric <danilosindric@gmail.com>
2025-07-01 20:06:59 +0200

    Add: reset after one day

---
+++ reset.js
@@ -0,0 +1,11 @@
+browser.storage.sync.get('solvedAt')
+    .then(({ solvedAt }) => {
+        const now = Date.now();
+        const oneDay = 1 * 24 * 60 * 1000;
+
+        if ((now - solvedAt) >= oneDay) {
+            browser.storage.sync.set({
+                isProblemSolved: false,
+            });
+        }
+    }, logError);
